c99cbcb7f0c4c3b7f8168744514a7d78743afdf9
xiu <gaoxiu333@gmail.com>
2026-03-18 20:03:44 +0800

    chore: remove apps/web placeholder app
    The web app was a template/example app that is no longer needed.

--- apps/web/src/app/provide.tsx
+++
@@ -1,3 +0,0 @@
-import { ThemeProvider } from 'next-themes'
-
-export const AppProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
